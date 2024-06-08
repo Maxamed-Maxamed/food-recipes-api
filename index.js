c808ef09e7f1b0ade183d266b63623467d3db133
Maxamed Maxamed <20088349@mail.wit.ie>
2024-06-08 12:32:17 +0100

    Setup basic routing for home and about pages

--- index.js
+++ index.js
@@ -1,30 +1,16 @@
-require('dotenv').config();
 const express = require('express');
 const bodyParser = require('body-parser');
-const axios = require('axios');
+const homeRouter = require('./routes/home');
+const aboutRouter = require('./routes/about');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
 
 app.use(bodyParser.json());
 
-app.get('/', (req, res) => {
-    res.send('Welcome to the Food Recipes API');
-});
-
-app.get('/recipe/:id', async (req, res) => {
-    const recipeId = req.params.id;
-    try {
-        const response = await axios.get(`https://api.spoonacular.com/recipes/${recipeId}/information`, {
-            params: {
-                apiKey: process.env.SPOONACULAR_API_KEY
-            }
-        });
-        res.json(response.data);
-    } catch (error) {
-        res.status(500).json({ error: error.message });
-    }
-});
+// Mount the route handlers
+app.use('/', homeRouter);
+app.use('/about', aboutRouter);
 
 app.listen(PORT, () => {
     console.log(`Server is running on port ${PORT}`);
